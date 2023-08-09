9634da4a417dce77e485a6d4d07b88ad3dbe1f09
aldunchev <aldunchev@gmail.com>
2023-08-09 07:43:56 +0000

    Deploying to gh-pages from @ aldunchev/stencil-storybook-token-demo@fd81e88b2c7e46d0bdff7b99a5b5a137ff0a4a12 🚀

--- docs/stencil-storybook-token-demo/stencil-storybook-token-demo.esm.js
+++ docs/stencil-storybook-token-demo/stencil-storybook-token-demo.esm.js
@@ -1,1 +1,1 @@
-import{b as a}from"./p-1cfe09be.js";import{p as b}from"./p-5a059910.js";b().then(b=>a([["p-097002ab",[[1,"my-button",{label:[1],size:[1],variant:[1],disabled:[4]}]]]],b));
+import{b as a}from"./p-1cfe09be.js";import{p as b}from"./p-5a059910.js";b().then(b=>a([["p-9d490e9b",[[1,"my-button",{label:[1],size:[1],variant:[1],disabled:[4]}]]]],b));
